463fd3b0540b696772002f8fd3d6b701383182b6
Zhang Junwei <m17610873365@163.com>
2025-03-21 16:36:28 +0800

    Modified nvim config
    Added plugin avante

--- nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -9,8 +9,8 @@ return {
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
-          statusline = {},
-          winbar = {},
+          statusline = { 'Avante', 'AvanteInput', 'AvanteSelectedFiles' },
+          winbar = { 'Avante', 'AvanteInput', 'AvanteSelectedFiles' },
         },
         ignore_focus = {},
         always_divide_middle = true,
