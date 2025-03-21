daf091421198ecd6250a7a59503ec7e83257368a
Zhang Junwei <m17610873365@163.com>
2025-03-21 16:37:58 +0800

    Modified nvim config
    Added windows zoom toggle feature using <C-z>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,8 +4,6 @@
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
 ========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
 ========         ||                    ||   | === |          ========
 ========         ||   KICKSTART.NVIM   ||   |-----|          ========
 ========         ||                    ||   | === |          ========
@@ -189,6 +187,42 @@ kmap('n', '<leader>fS', ':saveas ', { desc = '[f]ile [S]aveas' })
 
 --------  window operation (<leader>w) --------
 --  See `:help wincmd` for a list of all window commands
+--  below are some tool functions for windows zoomin (';z')
+local window_status = {}
+-- 自动清理窗口关闭后的残留状态
+vim.api.nvim_create_autocmd('WinClosed', {
+  pattern = '*',
+  callback = function(args)
+    local closed_winid = tonumber(args.match)
+    if closed_winid then
+      window_status[closed_winid] = nil
+    end
+  end,
+})
+local function toggle_zoom()
+  local winid = vim.api.nvim_get_current_win()
+  -- 初始化或获取窗口状态
+  window_status[winid] = window_status[winid]
+    or {
+      is_zoomed = false,
+      orig_height = vim.api.nvim_win_get_height(winid),
+      orig_width = vim.api.nvim_win_get_width(winid),
+    }
+  local state = window_status[winid]
+  if state.is_zoomed then
+    -- 恢复原始尺寸
+    vim.api.nvim_win_set_height(winid, state.orig_height)
+    vim.api.nvim_win_set_width(winid, state.orig_width)
+    state.is_zoomed = false
+  else
+    -- 保存当前尺寸并最大化
+    state.orig_height = vim.api.nvim_win_get_height(winid)
+    state.orig_width = vim.api.nvim_win_get_width(winid)
+    vim.cmd 'wincmd _|wincmd |' -- 合并最大化命令
+    state.is_zoomed = true
+  end
+end
+-- 映射 ;z 切换最大化/恢复
 kmap('n', ';-', ':split<CR>', { desc = 'split horizontal' })
 kmap('n', ';|', ':vsplit<CR>', { desc = 'split vertical' })
 kmap('n', ';h', '<C-w>h', { desc = 'change to left window' })
@@ -196,6 +230,7 @@ kmap('n', ';j', '<C-w>j', { desc = 'change to lower window' })
 kmap('n', ';k', '<C-w>k', { desc = 'change to upper window' })
 kmap('n', ';l', '<C-w>l', { desc = 'change to right window' })
 kmap('n', ';d', '<C-w>q', { desc = 'delete window' })
+kmap({ 'n', 'i', 't' }, '<C-z>', toggle_zoom, { desc = 'zoom window' })
 
 -------- code (<leader>c) --------
 kmap('n', '<leader>cc', 'gcc', { desc = '[c]ode toggle [c]omment', remap = true })
