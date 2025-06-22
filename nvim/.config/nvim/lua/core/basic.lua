c825a04315cebf67da85d0c72f0d6339db934ad4
zjw <m17610873365@163.com>
2025-06-22 23:04:29 +0800

    nvim: configured clipboard for wsl

--- nvim/.config/nvim/lua/core/basic.lua
+++ nvim/.config/nvim/lua/core/basic.lua
@@ -40,6 +40,23 @@ vim.opt.breakindent = true
 --  See `:help 'clipboard'`
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
+
+  if vim.fn.has('wsl') then
+    vim.cmd([[
+      let g:clipboard = {
+      \   'name': 'WslClipboard',
+      \   'copy': {
+      \     '+': 'clip.exe',
+      \     '*': 'clip.exe',
+      \   },
+      \   'paste': {
+      \     '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      \     '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      \   },
+      \   'cache_enabled': 0,
+      \ }
+    ]])
+  end
 end)
 
 -- set indent
