cb64305adea9ed94f81bf5f1a2c96f6d3e7b6f30
zjw <m17610873365@163.com>
2025-09-01 08:49:41 +0800

    feat(nvim): improved markdown rendering

--- nvim/.config/nvim/lua/plugins/lang/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/lang/render-markdown.lua
@@ -9,6 +9,40 @@ return {
   opts = {
     render_modes = true, -- Enable in all modes
     file_types = { 'markdown' },
-    completions = { blink = { enabled = true } },
+    sign = { enabled = false },
+    code = {
+      -- general
+      width = 'block',
+      min_width = 80,
+      -- borders
+      border = 'thin',
+      left_pad = 1,
+      right_pad = 1,
+      -- language info
+      position = 'right',
+      language_icon = true,
+      language_name = true,
+      -- prettier inline code in headings
+      highlight_inline = 'RenderMarkdownCodeInfo',
+    },
+    heading = {
+      border = true,
+    },
+    pipe_table = {
+      preset = 'round',
+    },
+    anti_conceal = {
+      disabled_modes = { 'n' },
+      ignore = {
+        bullet = true, -- render bullet in insert mode
+        head_border = true,
+        head_background = true,
+      },
+    },
+    win_options = { concealcursor = { rendered = 'vnc' } },
+    completions = {
+      blink = { enabled = true },
+      lsp = { enabled = true },
+    },
   },
 }
